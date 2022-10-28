d1b8ab7163b19f77bb838671a0794e0590f783b8
Anacia77 <edemdoamekpor7@gmail.com>
2022-10-28 19:53:51 +0000

    hng9i first commit

---
+++ endpoint/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.end, name='end'),
+    path('task1/', views.task1, name='task1'),
+]
---
+++ endpoint/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import json
+
+# Create your views here.
+def end(request):
+    return render(request, 'endpoint/end.html')
+
+def task1(request):
+    slackUsername = 'anacia'
+    age= int(25)
+    backend = True
+    bio = 'Web developer, Python developer'
+    data_set = {'slackUsername': slackUsername, 'backend': backend, 'age': age, 'bio': bio}
+    json_dump = json.dumps(data_set)
+    #return render(request, 'endpoint/task1.html', {'data_set': json_dump})
+    return HttpResponse(json_dump)
