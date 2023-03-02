876be99e801227c1ed7e557cb422e28df41eb752
Anacia77 <edemdoamekpor7@gmail.com>
2023-03-02 12:37:23 +0000

    Changed database to default

--- hng9/settings.py
+++ hng9/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'hng9.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         #'NAME': BASE_DIR / 'db.mysql',
@@ -91,8 +91,14 @@ DATABASES = {
         'PASSWORD': 'aiCanA'
 
     }
-}
+}'''
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
