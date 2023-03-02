f53f496e157420e59a7f12ccdf0dd38125b21ae3
Anacia77 <edemdoamekpor7@gmail.com>
2023-03-02 12:18:45 +0000

    railway setup

--- hng9/urls.py
+++ hng9/urls.py
@@ -29,6 +29,4 @@ urlpatterns = [
     path('weatherII/', include('weatherII.urls')),
     path('', include('user.urls')),
    
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
